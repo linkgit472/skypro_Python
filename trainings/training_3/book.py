b2386c5d04a56f1578a1d8780235fcfdaddfe7c6
Карина Эгельман <linkboygenshin@gmail.com>
2026-08-24 16:22:08 +0200

    Создана новая ветка для тренировочных заданий. Все тренировочные задания пересены в новую ветку

---
+++ trainings/training_3/book.py
@@ -0,0 +1,4 @@
+class Book:
+    def __init__(self, title, author):
+        self.bookTitle = title
+        self.bookAuthor = author
---
+++ trainings/training_3/ex1.py
@@ -0,0 +1,7 @@
+from product import Product
+
+my_product = Product('чипсы', 129)
+
+print(my_product.getName())
+print(my_product.getPrice())
+print(my_product.aboutProduct())
