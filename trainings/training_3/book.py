bd74c09875a2a9351e8d448c65327a7cd597ea76
Карина Эгельман <linkboygenshin@gmail.com>
2026-08-24 16:25:01 +0200

    Удаление перенесенной папки тренеровочных заданий из ветки lesson2

--- trainings/training_3/book.py
+++
@@ -1,4 +0,0 @@
-class Book:
-    def __init__(self, title, author):
-        self.bookTitle = title
-        self.bookAuthor = author
--- trainings/training_3/ex1.py
+++
@@ -1,7 +0,0 @@
-from product import Product
-
-my_product = Product('чипсы', 129)
-
-print(my_product.getName())
-print(my_product.getPrice())
-print(my_product.aboutProduct())
